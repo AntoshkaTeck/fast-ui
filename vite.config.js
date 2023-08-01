19969acd0f666349fcc583cdd79a52c93fe1cfd2
antoshka_teck <anton.tihov.94@gmail.com>
2023-08-01 16:21:46 +1200

    first commit

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import {fileURLToPath, URL} from 'node:url'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'FastUI',
+      fileName: 'fast-ui',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
